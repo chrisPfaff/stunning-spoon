f3b4ec0f1af5e50e713848ca0936a12e3b672207
chrisPfaff <chrispfaff10@gmail.com>
2023-03-15 23:47:44 -0500

    sending static files

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const app = require("express")();
-const port = 3000;
+const port = process.env.PORT || 8080;
 const path = require("path");
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/index.html"));
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.listen(port);
+console.log("Server started at http://localhost:" + port);
