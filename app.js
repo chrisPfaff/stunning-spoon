e7c85d9841f63f9c06a402c177cb52071bad5133
chrisPfaff <chrispfaff10@gmail.com>
2023-03-15 23:34:03 -0500

    making sure ci works

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const http = require("http");
 
-http.createServer(function(req, res){
-	res.write("Server working");
-	res.end();
-}).listen(3000);
+http
+  .createServer(function (req, res) {
+    res.write("Server working now");
+    res.end();
+  })
+  .listen(3000);
 
 console.log("server started");
-
