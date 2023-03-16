3e7e3cc6b3e9fd3bd2c662d642bcc28aa412b64b
chrisPfaff <chrispfaff10@gmail.com>
2023-03-16 08:47:37 -0500

    websocket

--- app.js
+++ app.js
@@ -8,3 +8,29 @@ app.get("/", (req, res) => {
 
 server.on("request", app);
 server.listen(3000, () => console.log("Listening on 3000"));
+
+const WebSocketServer = require("ws").Server;
+
+const wss = new WebSocketServer({ server: server });
+
+wss.on("connection", (ws) => {
+  const numClients = wss.clients.size;
+  console.log("Client connected. Total clients: " + numClients);
+
+  wss.broadcast("New client connected. Total clients: " + numClients);
+
+  if (ws.readyState === ws.OPEN) {
+    ws.send("Welcome to the chat!");
+  }
+
+  ws.on("close", () => {
+    wss.broadcast("New client connected. Total clients: " + numClients);
+    console.log("Client disconnected");
+  });
+});
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
